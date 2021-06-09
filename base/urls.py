dc54cf92ac944efcd362df6d03f95150aeb19ab1
YaRomanovIvan <romanov19972@yandex.ru>
2021-06-09 16:26:38 +0300

    fix

--- base/urls.py
+++ base/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
@@ -7,4 +6,19 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('viewing_block/', views.viewing_block, name='viewing_block'),
     path('records_block/', views.records_block, name='records_block'),
+    path(
+        "add_record_block/add_new_record_block/",
+        views.add_new_record_block,
+        name="add_new_record_block",
+    ),
+    path(
+        "add_record_block/add_new_type_block/",
+        views.add_new_type_block,
+        name="add_new_type_block",
+    ),
+    path(
+        "add_record_block/add_new_region/",
+        views.add_new_region,
+        name="add_new_region",
+    ),
 ]
